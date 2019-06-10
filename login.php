591a1b468daef22429f02979dc6ad80cc5fe6d64
dilan dogan <d.dogan@etu.edu.tr>
2019-06-10 23:45:26 +0300

    Login page and Register page are added.

---
+++ login.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+	<div class="header">
+		<h2>Login</h2>
+	</div>
+	
+	<form method="post" action="login.php">
+
+	<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_user">Login</button>
+		</div>
+		<p>
+			Not yet a member? <a href="register.php">Sign up</a>
+		</p>
+	</form>
+
+
+</body>
+</html>
