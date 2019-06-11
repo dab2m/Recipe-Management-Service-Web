57ba807c5cc76b46c0369108ef2f18feff084850
dilan dogan <d.dogan@etu.edu.tr>
2019-06-12 01:26:27 +0300

    login page modified

--- login.php
+++ login.php
@@ -1,30 +1,102 @@
+<?php
+    include 'db.php';
+    
+    if(isset($_POST['username']) && isset($_POST['password'])) 
+    {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $sql = "SELECT * FROM `kullanici` WHERE `username` = '$username' AND `password` = '$password'";
+        mysqli_query($db, $sql);
+        if(mysqli_affected_rows($db) > 0) {
+            session_start();
+            $_SESSION['username'] = $username;
+            $_SESSION['password'] = $password;
+            header("location:anasayfa.php");
+        }
+    }
+?>
 
 <!DOCTYPE html>
-<html>
-<body>
 
-	<div class="header">
-		<h2>Login</h2>
-	</div>
-	
-	<form method="post" action="login.php">
+<html lang="en">
+<meta charset="utf-8"/>
 
-	<div class="input-group">
-			<label>Username</label>
-			<input type="text" name="username" >
+<head>
+<meta charset="utf-8"/>
+<title>Giris</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta content="" name="description"/>
+<meta content="" name="author"/>
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
+<link href="assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
+
+
+<link href="assets/global/plugins/select2/select2.css" rel="stylesheet" type="text/css"/>
+<link href="assets/admin/pages/css/login3.css" rel="stylesheet" type="text/css"/>
+
+
+<link href="assets/global/css/components.css" id="style_components" rel="stylesheet" type="text/css"/>
+<link href="assets/global/css/plugins.css" rel="stylesheet" type="text/css"/>
+<link href="assets/admin/layout/css/layout.css" rel="stylesheet" type="text/css"/>
+<link href="assets/admin/layout/css/themes/default.css" rel="stylesheet" type="text/css" id="style_color"/>
+<link href="assets/admin/layout/css/custom.css" rel="stylesheet" type="text/css"/>
+
+<link rel="shortcut icon" href="favicon.ico"/>
+</head>
+
+<body class="login">
+
+<div class="logo">
+
+</div>
+
+<div class="menu-toggler sidebar-toggler">
+</div>
+
+<div class="content">
+	<!-- BEGIN LOGIN FORM -->
+	<form class="login-form" action="login.php" method="post">
+		<h3 class="form-title">Login</h3>
+		<div class="alert alert-danger display-hide">
+			<button class="close" data-close="alert"></button>
 		</div>
-		<div class="input-group">
-			<label>Password</label>
-			<input type="password" name="password">
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">Username</label>
+			<div class="input-icon">
+				<i class="fa fa-user"></i>
+				<input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username"/>
+			</div>
 		</div>
-		<div class="input-group">
-			<button type="submit" class="btn" name="login_user">Login</button>
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">Password</label>
+			<div class="input-icon">
+				<i class="fa fa-lock"></i>
+				<input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password"/>
+			</div>
 		</div>
-		<p>
-			Not yet a member? <a href="register.php">Sign up</a>
-		</p>
+		<div class="form-actions">
+			<label class="checkbox"></label>
+			<button type="submit" class="btn green-haze pull-right">
+			Giris <i class="m-icon-swapright m-icon-white"></i>
+			</button>
+		</div>
+		<div class="create-account">
+			<p>
+			<a href="javascript:;" id="register-btn">Register here </a>
+			</p>
+		</div>
 	</form>
 
-
+<script>
+jQuery(document).ready(function() {     
+  Login.init();
+  
+});
+</script>
 </body>
 </html>
