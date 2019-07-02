0d8a22265672a3e66a9bbd6b98695bf00408baa4
Theieyrre <crizfmtfm@gmail.com>
2019-07-02 09:01:43 +0300

    Adding port no. 3306

--- db.php
+++ db.php
@@ -1,5 +1,11 @@
 <?php
-    $db = mysqli_connect("127.0.0.1","root","","yemek_tarifi"); //girdigi link - db username - db password - db name
+    //$url = parse_url(getenv(mysql://be8001746b3ab5:25d67e82@us-cdbr-iron-east-02.cleardb.net/heroku_1f4b01bd817d3c2?reconnect=true));
+    //$server = $url["host"];
+    //$username = $url["user"];
+    //$password = $url["pass"];
+    //$database = substr($url["path"],1);
+    
+    $db = mysqli_connect("127.0.0.1:3306","root","","yemek_tarifi"); //girdigi link - db username - db password - db name 
     
     if (!$db) {
         echo "<script>alert('There is something wrong with DB connection...');</script>";
