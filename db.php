91c5f749ad35fac30c45449c332e176641053f16
berkayataeli <berkayataeli@gmail.com>
2019-07-03 16:55:42 +0300

    Changin parsing

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
-    $url = parse_url(getenv("mysql://be8001746b3ab5:25d67e82@us-cdbr-iron-east-02.cleardb.net/heroku_1f4b01bd817d3c2?reconnect=true"));
+    //$url = parse_url(getenv("mysql://be8001746b3ab5:25d67e82@us-cdbr-iron-east-02.cleardb.net/heroku_1f4b01bd817d3c2?reconnect=true"));
 
-    $server = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $conn = substr($url["path"],1);
+    $server = "us-cdbr-iron-east-02.cleardb.net";
+    $username = "be8001746b3ab5";
+    $password = "25d67e82";
+    $conn = "heroku_1f4b01bd817d3c2";
 
     //$db = mysqli_connect("127.0.0.1","root","","yemek_tarifi"); //girdigi link - db username - db password - db name 
     $db = mysqli_connect($server,$username,$password,$conn);
