b25d4b57695eef122b2b560811e76780f808726b
Theieyrre <crizfmtfm@gmail.com>
2019-07-03 15:21:15 +0300

    Connecting to ClearDB

--- db.php
+++ db.php
@@ -1,7 +1,14 @@
 <?php
+    $url = parse_url(getenv("mysql://be8001746b3ab5:25d67e82@us-cdbr-iron-east-02.cleardb.net/heroku_1f4b01bd817d3c2?reconnect=true"));
 
-    $db = mysqli_connect("127.0.0.1","root","","yemek_tarifi"); //girdigi link - db username - db password - db name 
-    
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $conn = substr($url["path"],1);
+
+    //$db = mysqli_connect("127.0.0.1","root","","yemek_tarifi"); //girdigi link - db username - db password - db name 
+    $db = mysqli_connect($server,$username,$password,$conn)
+
     if (!$db) {
         echo "<script>alert('There is something wrong with DB connection...');</script>";
         die("Connection failed: " . mysqli_connect_error());
