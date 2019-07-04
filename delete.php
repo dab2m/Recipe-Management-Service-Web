ae3d165dd03409965bfe1b6caf50801e54ad2207
berkayataeli <berkayataeli@gmail.com>
2019-07-04 22:50:53 +0300

    database deleting queries comleted

--- delete.php
+++ delete.php
@@ -6,10 +6,10 @@
 	$sql="DELETE FROM tarif WHERE id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
 	include 'db.php';
-	$sql="DELETE FROM tarif_tag WHERE tarif_id='".$_GET['del_id']."'";
+	$sql="DELETE FROM tag t INNER JOIN tarif_tag tt ON t.tag_id=tt.tag_id WHERE tarif_id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
 	include 'db.php';
-	$sql="DELETE FROM tag t INNER JOIN tarif_tag tt ON t.tag_id=tt.tag_id WHERE tarif_id='".$_GET['del_id']."'";
+	$sql="DELETE FROM tarif_tag WHERE tarif_id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
 	header("location:anasayfa.php");
 	?>
