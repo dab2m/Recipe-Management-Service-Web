e0771283c9f39a3e75da5c5c68ad8cba1f877587
berkayataeli <berkayataeli@gmail.com>
2019-07-04 22:29:16 +0300

    query bug fixes

--- delete.php
+++ delete.php
@@ -9,7 +9,7 @@
 	$sql="DELETE FROM tarif_tag WHERE tarif_id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
 	include 'db.php';
-	$sql="DELETE FROM tag INNER JOIN tarif_tag ON tarif_tag.tag_id=tag.tag_id WHERE tarif_id = '".$_GET['del_id']."'";
+	$sql="DELETE FROM tag WHERE SELECT tag_id FROM tarif_tag WHERE tarif_id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
 	header("location:anasayfa.php");
 	?>
