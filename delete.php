c762615f1c3892391ab5082e518d2480da1bfdae
berkayataeli <berkayataeli@gmail.com>
2019-07-04 22:23:23 +0300

    adding query

--- delete.php
+++ delete.php
@@ -8,6 +8,7 @@
 	include 'db.php';
 	$sql="DELETE FROM tarif_tag WHERE tarif_id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
+	include 'db.php';
 	$sql="DELETE FROM tag INNER JOIN tarif_tag ON tarif_tag.tag_id=tag.tag_id WHERE tarif_id = '".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
 	header("location:anasayfa.php");
