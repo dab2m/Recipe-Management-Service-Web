fcd10cdf562771134a2dd6754db7b63548ec8e67
berkayataeli <berkayataeli@gmail.com>
2019-07-04 22:21:03 +0300

    database queries updated

--- delete.php
+++ delete.php
@@ -5,6 +5,7 @@
 	include 'db.php';
 	$sql="DELETE FROM tarif WHERE id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
+	include 'db.php';
 	$sql="DELETE FROM tarif_tag WHERE tarif_id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
 	$sql="DELETE FROM tag INNER JOIN tarif_tag ON tarif_tag.tag_id=tag.tag_id WHERE tarif_id = '".$_GET['del_id']."'";
