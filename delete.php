c1e60e7184250a726d8675d96b1afb71347cb810
berkayataeli <berkayataeli@gmail.com>
2019-07-05 15:30:25 +0300

    tag_id delete

--- delete.php
+++ delete.php
@@ -6,7 +6,8 @@
 	$sql="DELETE FROM tarif WHERE id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
 	include 'db.php';
-	$sql="DELETE FROM tag t INNER JOIN tarif_tag tt ON t.tag_id=tt.tag_id WHERE tarif_id='".$_GET['del_id']."'";
+	//$sql="DELETE FROM tag t INNER JOIN tarif_tag tt ON t.tag_id=tt.tag_id WHERE tt.tarif_id='".$_GET['del_id']."'";
+	$sql="DELETE FROM tag WHERE tag_id = (SELECT tag_id FROM tarif_tag WHERE tarif_id='".$_GET['del_id']."')";
 	mysqli_query($db, $sql);
 	include 'db.php';
 	$sql="DELETE FROM tarif_tag WHERE tarif_id='".$_GET['del_id']."'";
