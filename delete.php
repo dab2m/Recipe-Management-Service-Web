cc402728a08122bf7121a170641090df2993bf72
Theieyrre <crizfmtfm@gmail.com>
2019-07-05 19:51:10 +0300

    Fixing inner join

--- delete.php
+++ delete.php
@@ -6,7 +6,7 @@
 	$sql="DELETE FROM tarif WHERE id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
 	//$sql="DELETE FROM tag t INNER JOIN tarif_tag tt ON t.tag_id=tt.tag_id WHERE tt.tarif_id='".$_GET['del_id']."'";
-	$sql="DELETE FROM tag INNER JOIN  tarif_tag ON tarif_tag.tag_id WHERE  tarif_id='".$_GET['del_id']."')";
+	$sql="DELETE FROM tag INNER JOIN  tarif_tag ON tarif_tag.tag_id=tag.tag_id WHERE  tarif_id='".$_GET['del_id']."')";
 	mysqli_query($db, $sql);
 	$sql="DELETE FROM tarif_tag WHERE tarif_id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
