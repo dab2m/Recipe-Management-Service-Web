0129fcd60f4d97b03c175744049d1f6d1981c0aa
Theieyrre <crizfmtfm@gmail.com>
2019-07-05 15:46:43 +0300

    Deleting useless folders

--- phpmyadmin/vendor/bin/codacycoverage.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../codacy/coverage/bin/codacycoverage
-php "%BIN_TARGET%" %*
--- phpmyadmin/vendor/bin/highlight-query.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../phpmyadmin/sql-parser/bin/highlight-query
-php "%BIN_TARGET%" %*
