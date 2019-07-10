963050727bed466d0e3bdd61492de34220f9d7de
Theieyrre <crizfmtfm@gmail.com>
2019-07-10 21:23:20 +0300

    nday bugfix

--- nday.php
+++ nday.php
@@ -1,9 +1,11 @@
 <?php
 include "db.php";
 $days = "SELECT * FROM 'n_gun'";
-$result = mysqli_query($db, $mysql);
+$result = mysqli_query($db, $days);
 if(mysqli_affected_rows($db) > 0)
     while($row = mysqli_fetch_assoc($result))
+    {
         $mysql = "DELETE FROM `tarif` WHERE `creation_date` < DATE_SUB(NOW(), INTERVAL " . $row["gun"] . " DAY) AND username=" . $row["username"];
         mysqli_query($db, $mysql);
+    }
 ?>
