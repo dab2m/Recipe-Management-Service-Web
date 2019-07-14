a0070da65be464aa4f6f9f662b5bd70d88f8b066
dilan dogan <d.dogan@etu.edu.tr>
2019-07-15 02:02:33 +0300

    nday is working

--- nday.php
+++ nday.php
@@ -6,9 +6,6 @@ if(mysqli_affected_rows($db) > 0)
     echo "If run";
     while($row = mysqli_fetch_assoc($result))
     {
-        echo "Hello form nday.php";
-        echo $row['gun'];
-        echo $row['username'];
         $mysql = "DELETE FROM `tarif` WHERE `creation_date` < DATE_SUB(NOW(), INTERVAL " . $row['gun'] . " DAY) AND username=\"" . $row['username'] . "\"";
         mysqli_query($db, $mysql);
     }
