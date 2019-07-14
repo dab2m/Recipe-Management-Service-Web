e407924b3790134b424e166173115e0622139d01
dilan dogan <d.dogan@etu.edu.tr>
2019-07-14 20:47:40 +0300

    fix 2

--- nday.php
+++ nday.php
@@ -2,10 +2,10 @@
 include "db.php";
 $days = "SELECT * FROM 'n_gun'";
 $result = mysqli_query($db, $days);
-echo "Hello form nday.php";
 if(mysqli_affected_rows($db) > 0)
     while($row = mysqli_fetch_assoc($result))
     {
+        echo "Hello form nday.php";
         echo $row['gun'];
         echo $row['username'];
         $mysql = "DELETE FROM `tarif` WHERE `creation_date` < DATE_SUB(NOW(), INTERVAL " . $row['gun'] . " DAY) AND username=" . $row['username'];
