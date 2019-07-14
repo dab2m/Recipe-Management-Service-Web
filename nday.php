170ae7d81dfb0ec63c6633e31d828f338ee0cf44
dilan dogan <d.dogan@etu.edu.tr>
2019-07-14 20:55:18 +0300

    delete query fixed

--- nday.php
+++ nday.php
@@ -9,7 +9,7 @@ if(mysqli_affected_rows($db) > 0)
         echo "Hello form nday.php";
         echo $row['gun'];
         echo $row['username'];
-        $mysql = "DELETE FROM `tarif` WHERE `creation_date` < DATE_SUB(NOW(), INTERVAL " . $row['gun'] . " DAY) AND username=" . $row['username'];
+        $mysql = "DELETE FROM `tarif` WHERE `creation_date` < DATE_SUB(NOW(), INTERVAL " . $row['gun'] . " DAY) AND username=\"" . $row['username'] . "\"";
         mysqli_query($db, $mysql);
     }
 ?>
