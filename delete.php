1f1c6c38b4b8eaa4d94a296317c58d59b3bc534a
dilan dogan <d.dogan@etu.edu.tr>
2019-07-16 14:25:10 +0300

    Delete from tag table when recipe is deleted

--- delete.php
+++ delete.php
@@ -15,10 +15,12 @@
 
 	$sql="DELETE FROM tarif WHERE id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
-	$sql="DELETE FROM tag t INNER JOIN tarif_tag tt ON t.tag_id=tt.tag_id WHERE tt.tarif_id='".$_GET['del_id']."'";
-	$sql="DELETE tag.* FROM tag INNER JOIN  tarif_tag ON tarif_tag.tag_id = tag.tag_id WHERE (tarif_id='".$_GET['del_id']."')";
-	mysqli_query($db, $sql);
+	//$sql="DELETE FROM tag t INNER JOIN tarif_tag tt ON t.tag_id=tt.tag_id WHERE tt.tarif_id='".$_GET['del_id']."'";
+	//$sql="DELETE tag.* FROM tag INNER JOIN  tarif_tag ON tarif_tag.tag_id = tag.tag_id WHERE (tarif_id='".$_GET['del_id']."')";
+	//mysqli_query($db, $sql);
 	$sql="DELETE FROM tarif_tag WHERE tarif_id='".$_GET['del_id']."'";
+	mysqli_query($db, $sql);
+	$sql="DELETE tag FROM tag INNER JOIN tarif_tag ON tag.tag_id=tarif_tag.tag_id WHERE tarif_id='".$_GET['del_id']."'";
 	mysqli_query($db, $sql);
 	header("location:tariflerim.php");
 	//removeCloud($_GET['del_id']);
