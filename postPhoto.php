f1e914c142478cf47e7c47096dc0efee56e4a4b1
Theieyrre <crizfmtfm@gmail.com>
2019-07-22 21:52:54 +0300

    postPhoto.php debug

--- postPhoto.php
+++ postPhoto.php
@@ -3,8 +3,17 @@
     header("Content-type: application/json");
     
     $image = file_get_contents('php://input');
-    $url = uploadCloud($image);
-    if(!empty($url))
+
+    $outjson = array(
+        "Image" => $image,
+        "var_dump" => var_dump($image),
+    );
+
+    echo json_encode($outjson);
+    
+    //$url = uploadCloud($image);
+
+    /*if(!empty($url))
     {
         $outjson = array(
             "Status" => "Success",
@@ -20,6 +29,6 @@
             "Trace" => "Image could not be uploaded",
         );
         echo json_encode($outjson);
-    }
+    }*/
 
 ?>
